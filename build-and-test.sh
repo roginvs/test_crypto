76014df716dfb31fdbeaa58b60617d0f4abf80f2
Vasilii Rogin <vasilii@giosg.com>
2020-02-19 21:27:33 +0200

    Adding tests

--- build-and-test.sh
+++ build-and-test.sh
@@ -8,21 +8,6 @@ gcc -Wall -o test.out test.c
 echo ""
 echo ""
 
-
-echo "Building executable"
-
-gcc -Wall -o main.out main.c -lpthread
-
-
-DIR=`mktemp -d`
-
-for fFull in `ls test/*`; do
-  f=`basename "${fFull}"`
-  echo "==== Testing $f file ===="
-  ./main.out "test/$f" "$DIR/$f.gz"
-  cat "$DIR/$f.gz" | gzip -d > "$DIR/$f.raw"
-  cmp "test/$f" "$DIR/$f.raw"
-  echo ""
-done
+## TODO: Build main executable and test it
 
 echo "All done"
--- poly8.test.c
+++ poly8.test.c
@@ -6,13 +6,20 @@
     if ((a) != (b))                                            \
     {                                                          \
         printf("\n");                                          \
-        printf("Asserted=0x%08x Obtained=0x%08x\n", (b), (a)); \
+        printf("Asserted=0x%02x Obtained=0x%02x\n", (b), (a)); \
         printf("Error with: ");                                \
         printf(msg);                                           \
         printf("\n");                                          \
         return 100;                                            \
     }
 ;
+
+#define assertMixColumn(d)                                                   \
+    assertEqual(mix_column_0(d[0], d[1], d[2], d[3]), d[4], "mix column 0"); \
+    assertEqual(mix_column_1(d[0], d[1], d[2], d[3]), d[5], "mix column 1"); \
+    assertEqual(mix_column_2(d[0], d[1], d[2], d[3]), d[6], "mix column 2"); \
+    assertEqual(mix_column_3(d[0], d[1], d[2], d[3]), d[7], "mix column 3"); \
+    ;
 
 int poly8_test()
 {
@@ -25,12 +32,11 @@ int poly8_test()
         _inverse_bits[0b10111010],
         "Some random poly");
 
-    assertEqual(mix_column_0(0xdb, 0x13, 0x53, 0x45), 0x8e, "mix_column_0");
-    assertEqual(mix_column_1(0xdb, 0x13, 0x53, 0x45), 0x4d, "mix_column_1");
-    assertEqual(mix_column_2(0xdb, 0x13, 0x53, 0x45), 0xa1, "mix_column_2");
-    assertEqual(mix_column_3(0xdb, 0x13, 0x53, 0x45), 0xbc, "mix_column_3");
+    uint8_t test1[] = {0xdb, 0x13, 0x53, 0x45, 0x8e, 0x4d, 0xa1, 0xbc};
+    assertMixColumn(test1);
 
-    assertEqual(mix_column_0(0xd4, 0xbf, 0x5d, 0x30), 0x04, "mix_column_0");
+    uint8_t test2[] = {0xd4, 0xbf, 0x5d, 0x30, 0x04, 0x66, 0x81, 0xe5};
+    assertMixColumn(test2);
 
     return 0;
 };
