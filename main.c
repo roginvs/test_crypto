e13879639f33707444171dc6fc261bba43e0d1af
Vasilii Rogin <vasilii@giosg.com>
2020-02-23 12:04:42 +0200

    Adding padding encryption

--- main.c
+++ main.c
@@ -22,10 +22,16 @@ void aes_256_encrypt(uint8_t *in, int32_t size, Block iv, uint8_t *key, FILE *ou
     memcpy(b, iv, BLOCK_SIZE);
 
     int32_t blocks_count = size / BLOCK_SIZE;
+    uint8_t tail_bytes_count = size % BLOCK_SIZE;
+    if (tail_bytes_count != 0)
+    {
+        blocks_count++;
+    };
+
     for (int32_t block_num = 0; block_num < blocks_count; block_num++)
     {
         uint8_t is_block_last = block_num == blocks_count - 1;
-        uint8_t tail_bytes_count = size % BLOCK_SIZE;
+
         uint8_t bytes_to_copy = is_block_last ? BLOCK_SIZE : tail_bytes_count == 0 ? BLOCK_SIZE : tail_bytes_count;
 
         printf("Block num = %i\n", block_num);
@@ -37,9 +43,11 @@ void aes_256_encrypt(uint8_t *in, int32_t size, Block iv, uint8_t *key, FILE *ou
             b[i] = b[i] ^ in[block_num * BLOCK_SIZE + i];
         };
 
+        // printf("=== %i %i %i\n", blocks_count, block_num, is_block_last);
         if (is_block_last)
         {
             uint8_t padding_count = BLOCK_SIZE - bytes_to_copy;
+
             for (uint8_t i = padding_count; i < BLOCK_SIZE; i++)
             {
                 b[i] = b[i] ^ padding_count;
@@ -48,16 +56,16 @@ void aes_256_encrypt(uint8_t *in, int32_t size, Block iv, uint8_t *key, FILE *ou
 
         aes_encrypt_block(b, key_expanded, AES_256_KEY_SIZE);
         fwrite(b, BLOCK_SIZE, 1, outfile);
+    };
 
-        if (is_block_last && tail_bytes_count == 0)
+    if (tail_bytes_count == 0)
+    {
+        for (uint8_t i = 0; i < BLOCK_SIZE; i++)
         {
-            for (uint8_t i = 0; i < BLOCK_SIZE; i++)
-            {
-                b[i] = b[i] ^ BLOCK_SIZE;
-            };
-            aes_encrypt_block(b, key_expanded, AES_256_KEY_SIZE);
-            fwrite(b, BLOCK_SIZE, 1, outfile);
-        }
+            b[i] = b[i] ^ BLOCK_SIZE;
+        };
+        aes_encrypt_block(b, key_expanded, AES_256_KEY_SIZE);
+        fwrite(b, BLOCK_SIZE, 1, outfile);
     };
 }
 
