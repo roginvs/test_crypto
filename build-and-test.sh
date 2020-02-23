a0dc968c7ab94027942a9669fe0019e0a54c5379
Vasilii Rogin <vasilii@giosg.com>
2020-02-23 12:13:59 +0200

    Add another key into tests

--- build-and-test.sh
+++ build-and-test.sh
@@ -17,8 +17,8 @@ DIR=`mktemp -d`
 for fFull in `ls testdata/*`; do
   f=`basename "${fFull}"`
   echo "==== Testing $f file ===="
-  #for key in "2b7e151628aed2a6abf7158809cf4f3c2b7e151628aed2a6abf7158809cf4f3c"; do
-  for key in "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f"; do
+  for key in "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f"\
+   "2b7e151628aed2a6abf7158809cf4f3c2b7e151628aed2a6abf7158809cf4f3c"; do
     echo "Using key=$key"
     for iv in "00000000000000000000000000000000" "22332211aabb22aa889922aa99002200"; do
       echo "Using iv=$iv"
