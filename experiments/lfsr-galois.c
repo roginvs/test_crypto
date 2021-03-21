ae9dfcedc4d928deec1d7f2c0a064c3672dd186f
Vasilii Rogin <vasilii@giosg.com>
2021-03-21 12:05:24 +0200

    Add polynoms

--- experiments/lfsr-galois.c
+++ experiments/lfsr-galois.c
@@ -15,18 +15,21 @@ void test_lfsr()
     POLYNOM start_state = 0x1; /* Any nonzero start state will work. */
     POLYNOM lfsr = start_state;
 
-    // This is a "1 + x + x^n" polynom
+    // This is a "x^n + x^1 + 1" polynom
     // For 16 bits it is equal to 0b1100000000000000
-    // Lowers degree is highest bit
+    // (Lowest degree is highest bit)
     POLYNOM const polynom_mask = (POLYNOM)0b11 << ((sizeof(POLYNOM) * CHAR_BIT) - 2);
 
     // This is an example from wikipedia, deg=16
-    //POLYNOM const polynom_mask = 0b1011010000000000;
+    // x^16 + x^5 + x^3 + x^2 + 1
+    // POLYNOM const polynom_mask = 0b1011010000000000;
 
     // An irreducible polynomial from
     // https://www.partow.net/programming/polynomials/index.html#deg16
     // x^16 + x^12 + x^3 + x^1 + 1
     // POLYNOM const polynom_mask = 0b1101000000001000;
+    // x^16 + x^15 + x^11 + x^10 + x^9 + x^8 + x^6 + x^4 + x^2 + x^1 + 1
+    // POLYNOM const polynom_mask = 0b1110101011110001;
 
     unsigned long period = 0;
 
