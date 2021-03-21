e9bb1cfdda00b66729c036e4391c4d804f1b148e
Vasilii Rogin <vasilii@giosg.com>
2021-03-21 12:13:53 +0200

    Uncomment polynoms

--- experiments/lfsr-galois.c
+++ experiments/lfsr-galois.c
@@ -18,18 +18,21 @@ void test_lfsr()
     // This is a "x^n + x^1 + 1" polynom
     // For 16 bits it is equal to 0b1100000000000000
     // (Lowest degree is highest bit)
-    POLYNOM const polynom_mask = (POLYNOM)0b11 << ((sizeof(POLYNOM) * CHAR_BIT) - 2);
+    POLYNOM const polynom_mask_demo = (POLYNOM)0b11 << ((sizeof(POLYNOM) * CHAR_BIT) - 2);
 
     // This is an example from wikipedia, deg=16
     // x^16 + x^5 + x^3 + x^2 + 1
-    // POLYNOM const polynom_mask = 0b1011010000000000;
+    POLYNOM const polynom_mask_wiki = 0b1011010000000000;
 
     // An irreducible polynomial from
     // https://www.partow.net/programming/polynomials/index.html#deg16
     // x^16 + x^12 + x^3 + x^1 + 1
-    // POLYNOM const polynom_mask = 0b1101000000001000;
+    POLYNOM const polynom_mask_irreducible_1 = 0b1101000000001000;
     // x^16 + x^15 + x^11 + x^10 + x^9 + x^8 + x^6 + x^4 + x^2 + x^1 + 1
-    // POLYNOM const polynom_mask = 0b1110101011110001;
+    POLYNOM const polynom_mask_irreducible_2 = 0b1110101011110001;
+
+    // Let's try to use this one!
+    POLYNOM const polynom_mask = polynom_mask_demo;
 
     unsigned long observed_period = 0;
 
